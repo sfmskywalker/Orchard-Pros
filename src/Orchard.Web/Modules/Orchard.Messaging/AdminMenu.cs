8313ecca9353679d3eb4744828d739d66fb1cd77
Sipke Schoorstra <skywalker@darkskydevelopment.net>
2013-10-13 12:09:43 +0200

    Adding Admin Menu.

---
+++ src/Orchard.Web/Modules/Orchard.Messaging/AdminMenu.cs
@@ -0,0 +1,14 @@
+﻿using Orchard.Environment.Extensions;
+using Orchard.UI.Navigation;
+
+namespace Orchard.Messaging {
+    [OrchardFeature("Orchard.Messaging.Queuing")]
+    public class AdminMenu : Component, INavigationProvider {
+        public string MenuName { get { return "admin"; } }
+
+        public void GetNavigation(NavigationBuilder builder) {
+            builder.Add(T("Messaging"), "5.0", item => item
+                .Action("Index", "AdminQueue", new { area = "Orchard.Messaging" }));
+        }
+    }
+}
