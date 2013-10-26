fc4da5bab2222a6d9114a88b5dc335a2b9c1a969
Sipke Schoorstra <skywalker@darkskydevelopment.net>
2013-10-26 10:44:31 +0200

    Adding Templates admin menu.

---
+++ src/Orchard.Web/Modules/Orchard.Templates/AdminMenu.cs
@@ -0,0 +1,11 @@
+﻿using Orchard.UI.Navigation;
+
+namespace Orchard.Templates {
+    public class AdminMenu : Component, INavigationProvider {
+        public string MenuName { get { return "admin"; } }
+
+        public void GetNavigation(NavigationBuilder builder) {
+            builder.Add(T("Templates"), "5.0", item => item.Action("List", "Admin", new {area = "Orchard.Templates"}));
+        }
+    }
+}
