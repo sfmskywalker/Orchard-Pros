d7e743e8ec15e2ceeb29d9a9dbedfc63cc3b5956
Sipke Schoorstra <skywalker@darkskydevelopment.net>
2013-10-29 16:24:32 +0100

    Further mocking HttpContext for background context support.

--- src/Orchard.Web/Modules/Orchard.Templates/Services/DefaultTemplateService.cs
+++ src/Orchard.Web/Modules/Orchard.Templates/Services/DefaultTemplateService.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
-using System.Web.Routing;
 using Orchard.ContentManagement;
 using Orchard.DisplayManagement;
 using Orchard.DisplayManagement.Implementation;
+using Orchard.Mvc;
 using Orchard.Templates.Models;
 
 namespace Orchard.Templates.Services {
@@ -15,18 +16,21 @@ namespace Orchard.Templates.Services {
         private readonly IWorkContextAccessor _workContextAccessor;
         private readonly IContentManager _contentManager;
         private readonly IEnumerable<ITemplateProcessor> _processors;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public DefaultTemplateService(
             IShapeFactory shapeFactory, 
             IDisplayHelperFactory displayHelperFactory, 
             IWorkContextAccessor workContextAccessor, 
             IContentManager contentManager, 
-            IEnumerable<ITemplateProcessor> processors) {
+            IEnumerable<ITemplateProcessor> processors,
+            IHttpContextAccessor httpContextAccessor) {
             _shapeFactory = shapeFactory;
             _displayHelperFactory = displayHelperFactory;
             _workContextAccessor = workContextAccessor;
             _contentManager = contentManager;
             _processors = processors;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public string ExecuteShape(string shapeType) {
@@ -35,10 +39,15 @@ namespace Orchard.Templates.Services {
 
         public string ExecuteShape(string shapeType, INamedEnumerable<object> parameters) {
             var shape = _shapeFactory.Create(shapeType, parameters);
-            var viewContext = new ViewContext {HttpContext = _workContextAccessor.GetContext().HttpContext};
-            viewContext.RouteData.DataTokens["IWorkContextAccessor"] = _workContextAccessor;
-            var display = _displayHelperFactory.CreateHelper(viewContext, new ViewDataContainer());
-            var result = ((DisplayHelper)display).ShapeExecute(shape).ToString();
+            var result = "";
+
+            ExecuteInHttpContext(httpContext => {
+                var viewContext = new ViewContext { HttpContext = httpContext };
+                viewContext.RouteData.DataTokens["IWorkContextAccessor"] = _workContextAccessor;
+                var display = _displayHelperFactory.CreateHelper(viewContext, new ViewDataContainer());
+                result = ((DisplayHelper)display).ShapeExecute(shape).ToString();
+            });
+            
             return result;
         }
 
@@ -53,6 +62,26 @@ namespace Orchard.Templates.Services {
 
         public IEnumerable<ShapePart> GetTemplates(VersionOptions versionOptions = null) {
             return _contentManager.Query<ShapePart>(versionOptions ?? VersionOptions.Published).List();
+        }
+
+        /// <summary>
+        /// Executes the action in an HttpContext, regardless of whether or not we're in a ThreadStaticScope or HttpContextScope.
+        /// </summary>
+        private void ExecuteInHttpContext(Action<HttpContextBase> action) {
+            var httpContext = _httpContextAccessor.Current();
+
+            if (httpContext != null) {
+                action(httpContext);
+                return;
+            }
+
+            // We're not using the ViewContext.HttpContext because that will be an EmptyHttpContext when we're on a background thread.
+            // EmptyHttpContext does not implement Items, which is necessary for Orchard when storing and accessing certain services such as IWorkContextAccessor.
+            httpContext = new StubHttpContext();
+            using (var scope = _workContextAccessor.CreateWorkContextScope(httpContext)) {
+                _httpContextAccessor.Set(httpContext);
+                action(httpContext);
+            }
         }
 
         private class ViewDataContainer : IViewDataContainer {
