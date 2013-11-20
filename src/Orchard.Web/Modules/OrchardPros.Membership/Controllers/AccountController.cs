c6ff310373e2fde58be522a93791f992a96ba553
Sipke Schoorstra <sipke@ideliverable.net>
2013-11-20 14:38:57 +0100

    Incremental work on membership.

---
+++ src/Orchard.Web/Modules/OrchardPros.Membership/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+using Orchard.Themes;
+
+namespace OrchardPros.Membership.Controllers {
+    [Themed]
+    public class AccountController : Controller {
+        public ActionResult SignUp() {
+            return View();
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/OrchardPros.Membership/Helpers/UrlExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Mvc;
+
+namespace OrchardPros.Membership.Helpers {
+    public static class UrlExtensions {
+        public const string Area = "OrchardPros.Membership";
+        public static string AccountSignUp(this UrlHelper url) {
+            return url.Action("SignUp", "Account", new {Area});
+        }
+    }
+}
