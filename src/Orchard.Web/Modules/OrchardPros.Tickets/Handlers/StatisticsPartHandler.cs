8572075c63d08a0c3c2d8484441af569cf810e30
Sipke Schoorstra <sipke@ideliverable.net>
2014-01-01 11:15:45 +0100

    Implemented ViewCount.

--- src/Orchard.Web/Modules/OrchardPros.Tickets/Handlers/StatisticsPartHandler.cs
+++ src/Orchard.Web/Modules/OrchardPros.Tickets/Handlers/StatisticsPartHandler.cs
@@ -6,6 +6,11 @@ namespace OrchardPros.Tickets.Handlers {
     public class StatisticsPartHandler : ContentHandler {
         public StatisticsPartHandler(IRepository<StatisticsPartRecord> repository) {
             Filters.Add(StorageFilter.For(repository));
+            OnGetDisplayShape<StatisticsPart>(UpdateStatistics);
+        }
+
+        private void UpdateStatistics(BuildDisplayContext context, StatisticsPart part) {
+            part.ViewCount++;
         }
     }
 }
