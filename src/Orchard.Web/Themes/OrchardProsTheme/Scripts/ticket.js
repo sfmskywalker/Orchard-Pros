684c99b8dd855200ebe43bf3bd25b9ff16dfcde2
Sipke Schoorstra <sipke@ideliverable.net>
2014-01-05 16:37:29 +0100

    Implemented Reply buttons.

--- src/Orchard.Web/Themes/OrchardProsTheme/Scripts/ticket.js
+++ src/Orchard.Web/Themes/OrchardProsTheme/Scripts/ticket.js
@@ -1,4 +1,14 @@
-(function($) {
+(function ($) {
+
+    var smoothScroll = function(target) {
+        var scrollToElement = $(target);
+
+        var offset = scrollToElement.offset().top;
+        $("html:not(:animated),body:not(:animated)").animate({ scrollTop: offset }, 500, function () {
+            window.location.hash = target;
+        });
+    };
+
     $(function() {
         $("#replies").on("click", ".btn-solve-ticket", function (e) {
             var button = $(this);
@@ -21,6 +31,22 @@
         $(".overlay .popup .close").on("click", function () {
             var overlay = $(this).parents(".overlay:first");
             overlay.hide();
+        });
+
+        $("body").on("click", "[data-reply-subject]", function (e) {
+            e.preventDefault();
+
+            var link = $(this);
+            var replyId = link.data("reply-id");
+            var subject = link.data("reply-subject");
+
+            $("#post-reply input[name='Title']").val(subject);
+
+            if (replyId) {
+                $("#post-reply input[name='ContentItemId']").val(replyId);
+            }
+
+            smoothScroll(link.attr("href"));
         });
     });
 })(jQuery);
