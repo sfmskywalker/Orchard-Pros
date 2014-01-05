d21bb6299e70cba6324d3fae5dccbbcb78403c99
Sipke Schoorstra <sipke@ideliverable.net>
2014-01-05 17:16:14 +0100

    Implemented Reply model validation.

--- src/Orchard.Web/Modules/OrchardPros/ViewModels/ReplyViewModel.cs
+++ src/Orchard.Web/Modules/OrchardPros/ViewModels/ReplyViewModel.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OrchardPros.ViewModels {
     public class ReplyViewModel {
         public int ContentItemId { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Body { get; set; }
         public int? ParentReplyId { get; set; }
         public AttachmentsViewModel Attachments { get; set; }
