72e40cdb86798256e5d527748546337b28251b19
Sipke Schoorstra <sipke@ideliverable.net>
2014-01-11 20:07:01 +0100

    Fixed a bug.

--- src/Orchard.Web/Modules/OrchardPros/Events/AssignExperienceTicketEventHandler.cs
+++ src/Orchard.Web/Modules/OrchardPros/Events/AssignExperienceTicketEventHandler.cs
@@ -4,8 +4,14 @@ using OrchardPros.Services;
 
 namespace OrchardPros.Events {
     public class AssignExperienceTicketEventHandler : ITicketEventHandler {
+        private readonly IUserManager _userManager;
+
+        public AssignExperienceTicketEventHandler(IUserManager userManager) {
+            _userManager = userManager;
+        }
+
         public void Solved(TicketSolvedContext context) {
-            context.Expert.As<UserProfilePart>().ExperiencePoints += context.Ticket.ExperiencePoints;
+            _userManager.AddXp(context.Expert.As<UserProfilePart>(), context.Ticket.ExperiencePoints);
         }
     }
 }
